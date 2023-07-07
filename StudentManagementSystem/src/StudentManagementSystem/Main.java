a0c30fada9d84a9ccf28f49557e787d78b3f9432
LOGA-KRISHNA <130237929+LOGA-KRISHNA@users.noreply.github.com>
2023-07-07 23:22:02 +0530

    Add files via upload

---
+++ StudentManagementSystem/src/StudentManagementSystem/Main.java
@@ -0,0 +1,11 @@
+package StudentManagementSystem;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        AppGUI window = new AppGUI();
+        window.pack();
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
